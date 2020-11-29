bb7b678e710b949af32891c38d03e7696506526b
Aidan Shimmon <aidanshimmon@aidans-mbp.home>
2020-11-29 22:39:42 +0000

    setting up router

--- src/App.js
+++ src/App.js
@@ -2,13 +2,29 @@ import GlobalStyles from "./components/GlobalStyle";
 
 import AboutUs from "./pages/AboutUs";
 import Nav from "./components/Nav";
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
+// Router
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyles />
       <Nav />
-      <AboutUs />
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs />
+        </Route>
+
+        <Route path="/work">
+          <OurWork />
+        </Route>
+
+        <Route path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
