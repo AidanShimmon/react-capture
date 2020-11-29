c8f059e1b5fdfbc128c984535464d23f9e502de8
Aidan Shimmon <aidanshimmon@aidans-mbp.home>
2020-11-29 21:27:45 +0000

    started styled components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import GlobalStyles from "./components/GlobalStyle";
+
 import AboutUs from "./ pages/AboutUs";
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyles />
       <AboutUs />
     </div>
   );
