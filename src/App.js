039b86866c52ba35061fe8dc97b76d7c1190eed8
Aidan Shimmon <aidanshimmon@aidans-mbp.home>
2020-11-29 22:23:35 +0000

    adding the nav

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import GlobalStyles from "./components/GlobalStyle";
 
-import AboutUs from "./ pages/AboutUs";
+import AboutUs from "./pages/AboutUs";
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyles />
+      <Nav />
       <AboutUs />
     </div>
   );
