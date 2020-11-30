310f6ddeb08fabbcb30b05ad437f82130ac206bb
Aidan Shimmon <aidanshimmon@aidans-mbp.home>
2020-11-30 13:00:44 +0000

    contact us page styles

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,60 +1,70 @@
-import { useState } from "react";
 import styled from "styled-components";
-import { About } from "../styles";
+import { About, Hide } from "../styles";
 import Toggle from "./Toggle";
 import { AnimateSharedLayout } from "framer-motion";
+import { scrollReveal } from "../animation";
+import { useScroll } from "./useScroll";
 
 const FaqSection = () => {
+  const [element, controls] = useScroll();
   return (
-    <Faq>
-      <h2>
-        Any Questions <span>FAQ</span>
-      </h2>
-      <AnimateSharedLayout>
-        <Toggle title="How Do I Start?">
-          <div className="answer">
-            <p>Lorem ipsum dolor sit amet.</p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi non
-              error porro sapiente sequi incidunt totam quaerat, ipsum
-              repellendus rem?
-            </p>
-          </div>
-        </Toggle>
-        <Toggle title="Daily Schedule">
-          <div className="answer">
-            <p>Lorem ipsum dolor sit amet.</p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia,
-              accusamus!
-            </p>
-          </div>
-        </Toggle>
-        <Toggle title="Different Payment Methods">
-          <div className="answer">
-            <p>Lorem ipsum dolor sit amet.</p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia,
-              accusamus!
-            </p>
-          </div>
-        </Toggle>
-        <Toggle title="What Products do you offer?">
-          <div className="answer">
-            <p>Lorem ipsum dolor sit amet.</p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia,
-              accusamus!
-            </p>
-          </div>
-        </Toggle>
-      </AnimateSharedLayout>
-    </Faq>
+    <Hide>
+      <Faq
+        variants={scrollReveal}
+        animate={controls}
+        initial="hidden"
+        ref={element}
+      >
+        <h2>
+          Any Questions <span>FAQ</span>
+        </h2>
+        <AnimateSharedLayout>
+          <Toggle title="How Do I Start?">
+            <div className="answer">
+              <p>Lorem ipsum dolor sit amet.</p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi
+                non error porro sapiente sequi incidunt totam quaerat, ipsum
+                repellendus rem?
+              </p>
+            </div>
+          </Toggle>
+          <Toggle title="Daily Schedule">
+            <div className="answer">
+              <p>Lorem ipsum dolor sit amet.</p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Mollitia, accusamus!
+              </p>
+            </div>
+          </Toggle>
+          <Toggle title="Different Payment Methods">
+            <div className="answer">
+              <p>Lorem ipsum dolor sit amet.</p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Mollitia, accusamus!
+              </p>
+            </div>
+          </Toggle>
+          <Toggle title="What Products do you offer?">
+            <div className="answer">
+              <p>Lorem ipsum dolor sit amet.</p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Mollitia, accusamus!
+              </p>
+            </div>
+          </Toggle>
+        </AnimateSharedLayout>
+      </Faq>
+    </Hide>
   );
 };
 
 const Faq = styled(About)`
   display: block;
+  overflow: hidden;
   span {
     display: block;
   }
